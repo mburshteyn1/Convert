2afad17c1ef73e2cff59623c44c2f2d3f725e6ae
Mikhail Burshteyn <mikhailburshteyn@Mikhails-MacBook-Pro.local>
2015-03-20 21:35:36 -0400

    Initial Commit

---
+++ Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Converter
+//
+//  Created by Mikhail Burshteyn on 3/20/15.
+//  Copyright (c) 2015 Mikhail Burshteyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
