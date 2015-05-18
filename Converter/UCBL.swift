f10c02d6cf9cef55e9dd8cd6716c03744f3e24ff
Mikhail Burshteyn <mikhailburshteyn@Mikhails-MacBook-Pro.local>
2015-05-18 09:45:21 -0400

    Change Copyright

--- Converter/UCBL.swift
+++ Converter/UCBL.swift
@@ -3,7 +3,7 @@
 //  Convert
 //
 //  Created by Mikhail Burshteyn on 12/2/14.
-//  Copyright (c) 2014 Southern Company. All rights reserved.
+//  Copyright (c) 2015 Mikhail Burshteyn. All rights reserved.
 //
 
 import Foundation
